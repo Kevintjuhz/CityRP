21fccd200e355f319833a1d001f201908225bec7
Andy Wong <andrewandy2003a@gmail.com>
2020-04-06 18:22:52 +0200

    Constructor clone on getCities in CityCache

--- src/main/java/nl/kqcreations/cityrp/city/CityCache.java
+++ src/main/java/nl/kqcreations/cityrp/city/CityCache.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
+import org.mineacademy.fo.Valid;
 import org.mineacademy.fo.collection.SerializedMap;
 import org.mineacademy.fo.model.ConfigSerializable;
 import org.mineacademy.fo.settings.YamlSectionConfig;
@@ -13,29 +14,33 @@ import java.util.*;
 @Getter
 public class CityCache extends YamlSectionConfig {
 
-	private static final Map<World, CityCache> cacheMap = new HashMap<>();
+    private static final Map<World, CityCache> cacheMap = new HashMap<>();
 
-	private final World world;
-	private Collection<City> cities = new HashSet<>();
+    private final World world;
+    private Collection<City> cities = new HashSet<>();
 
 
-	public CityCache(final World world) {
-		super(world.getName());
-		this.world = world;
+    public CityCache(final World world) {
+        super(world.getName());
+        this.world = world;
 
-		loadConfiguration(NO_DEFAULT, "data/cities.yml");
-		cacheMap.put(world, this);
+        loadConfiguration(NO_DEFAULT, "data/cities.yml");
+        cacheMap.put(world, this);
+    }
+
+    @Override
+    protected void onLoadFinish() {
+        cities = getSetSafe("Cities", City.class);
+    }
+
+    public Collection<City> getCities() {
+    	return new HashSet<>(cities);
 	}
 
-	@Override
-	protected void onLoadFinish() {
-		cities = getSetSafe("Cities", City.class);
-	}
 
-
-	// --------------------------------------------------------------------------------------------------------------
-	// Static methods below
-	// --------------------------------------------------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------------------------
+    // Static methods below
+    // --------------------------------------------------------------------------------------------------------------
 
 //	public static void addCity(final World world, final String name) {
 //		addCity(world, name, ChatColor.WHITE);
@@ -45,23 +50,23 @@ public class CityCache extends YamlSectionConfig {
 //		addCity(world, name, color, "");
 //	}
 
-	public static void addCity(final World world, final String name, final String wg_region) {
-		addCity(world, name, wg_region, ChatColor.WHITE);
-	}
+    public static void addCity(final World world, final String name, final String wg_region) {
+        addCity(world, name, wg_region, ChatColor.WHITE);
+    }
 
-	public static void addCity(final World world, final String name, final String wg_region, final ChatColor color) {
-		CityCache cache = cacheMap.get(world);
+    public static void addCity(final World world, final String name, final String wg_region, final ChatColor color) {
+        CityCache cache = cacheMap.get(world);
 
-		if (cache == null) {
-			cache = new CityCache(world);
-			cacheMap.put(world, cache);
-		}
+        if (cache == null) {
+            cache = new CityCache(world);
+            cacheMap.put(world, cache);
+        }
 
-		cache.getCities().add(new City(name, wg_region, color));
-	}
+        cache.cities.add(new City(name, wg_region, color));
+    }
 
-	public static CityCache getCityCache(final World world) {
-		CityCache cache = cacheMap.get(world);
+    public static CityCache getCityCache(final World world) {
+        CityCache cache = cacheMap.get(world);
 
 //		if (cache == null) {
 //			cache = new CityCache(world);
@@ -69,59 +74,59 @@ public class CityCache extends YamlSectionConfig {
 //			cacheMap.put(world, cache);
 //		}
 
-		return cache;
-	}
+        return cache;
+    }
 
-	public static Optional<City> getCityByName(final World world, final String name) {
-		CityCache cache = cacheMap.get(world);
+    public static Optional<City> getCityByName(final World world, final String name) {
+        CityCache cache = cacheMap.get(world);
 
-		for (final City city : cache.getCities())
-			if (city.getName().equals(name)) {
-				return Optional.of(city);
-			}
+        for (final City city : cache.getCities())
+            if (city.getName().equals(name)) {
+                return Optional.of(city);
+            }
 
 
-		return Optional.empty();
-	}
+        return Optional.empty();
+    }
 
-	public static Collection<City> getCities(World world) {
-		CityCache cache = getCityCache(world);
-		Valid.checkNotNull(cache, "This world is not valid");
+    public static Collection<City> getCities(World world) {
+        CityCache cache = getCityCache(world);
+        Valid.checkNotNull(cache, "This world is not valid");
 
-		return cache.getCities();
-	}
+        return cache.getCities();
+    }
 
-	// --------------------------------------------------------------------------------------------------------------
-	// Own classes
-	// --------------------------------------------------------------------------------------------------------------
+    // --------------------------------------------------------------------------------------------------------------
+    // Own classes
+    // --------------------------------------------------------------------------------------------------------------
 
-	@Getter
-	@AllArgsConstructor
-	public final static class City implements ConfigSerializable {
+    @Getter
+    @AllArgsConstructor
+    public final static class City implements ConfigSerializable {
 
-		private String name;
-		private String wg_region;
-		private ChatColor color;
+        private String name;
+        private String wg_region;
+        private ChatColor color;
 
-		@Override
-		public SerializedMap serialize() {
-			final SerializedMap map = new SerializedMap();
+        @Override
+        public SerializedMap serialize() {
+            final SerializedMap map = new SerializedMap();
 
-			map.put("Name", name);
-			map.put("WG_Region", wg_region);
-			map.put("Color", color.name());
+            map.put("Name", name);
+            map.put("WG_Region", wg_region);
+            map.put("Color", color.name());
 
-			return map;
-		}
+            return map;
+        }
 
-		public static City deserialize(final SerializedMap map) {
-			final String name = map.getString("Name");
-			final String wg_region = map.getString("WG_Region");
-			final ChatColor color = map.get("Color", ChatColor.class);
+        public static City deserialize(final SerializedMap map) {
+            final String name = map.getString("Name");
+            final String wg_region = map.getString("WG_Region");
+            final ChatColor color = map.get("Color", ChatColor.class);
 
-			return new City(name, wg_region, color);
-		}
+            return new City(name, wg_region, color);
+        }
 
-	}
+    }
 
 }
