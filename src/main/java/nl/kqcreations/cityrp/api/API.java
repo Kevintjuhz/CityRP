1b2a008bffe65c3d5773a56d773839c2b4ec47f7
Andy Wong <andrewandy2003a@gmail.com>
2020-04-07 00:30:59 +0200

    Add an API method to load banks

--- src/main/java/nl/kqcreations/cityrp/api/API.java
+++ src/main/java/nl/kqcreations/cityrp/api/API.java
@@ -1,4 +1,11 @@
 package nl.kqcreations.cityrp.api;
 
+import nl.kqcreations.cityrp.banking.BankDatabase;
+
 public class API {
+
+    public void loadBanking() {
+        BankDatabase.getInstance().loadBankAccounts(true);
+    }
+
 }
