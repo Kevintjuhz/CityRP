6b49a8f3eabdc267a747b843ca860896aedbd4a1
Andy Wong <andrewandy2003a@gmail.com>
2020-04-08 20:22:35 +0200

    Load Banking on plugin start:

--- src/main/java/nl/kqcreations/cityrp/CityRPPlugin.java
+++ src/main/java/nl/kqcreations/cityrp/CityRPPlugin.java
@@ -48,7 +48,7 @@ public class CityRPPlugin extends SimplePlugin {
             }
 
             registerCommands("city", new CityCommandGroup());
-            API.loadBanking();
+            API.loadAll();
             registerEvents(new PlayerListener());
         }
     }
